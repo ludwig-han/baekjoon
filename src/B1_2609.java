7213ed700611de229b9bc9c1fedc09930d036807
ludwig-han <ludwighan.dev@gmail.com>
2026-01-08 01:49:21 +0900

    260108: B1_2609 // gcd

---
+++ src/B1_2609.java
@@ -0,0 +1,35 @@
+// https://www.acmicpc.net/problem/2609
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1_2609 {
+    static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(line);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int max;
+        int min;
+
+        if (a < b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        min = a * b;
+        while (a % b != 0) {
+            int tmp = a;
+            a = b;
+            b = tmp % b;
+        }
+        max = b;
+        min = min / max;
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
