a23c32939e38ec93116fc28df85fa2fea2149d6f
ludwig-han <ludwighan.dev@gmail.com>
2026-07-04 21:36:51 +0900

    20260704: Review (유연근무제)

--- src/lv1/Review.java
+++ src/lv1/Review.java
@@ -1,62 +1,35 @@
 package lv1;
-// 1. completeRow(꽉 찬 줄 개수) 구하기
-// 2. numRow(num이 속한 줄) 구하기
-// 3. 최소 answer = complelteRow - numRow
-// 4. 맨 윗 줄이 꽉찬 줄일 경우 (n % w == 0) => answer 반환
-// 5. numDistance, lastDistance 정의 (가장 왼쪽 세로 줄로부터의 거리)
-// 6. answer의 1 증가 조건:
-// ((last줄 좌향) and (numDistance > lastDistance))
-// or ((last줄 우향) and (numDistance < lastDistance))
-// or (numDistance == lastDistance)
+
 class Review {
     public static void main(String[] args) {
         Review rv = new Review();
-        int[][] testCase = {
-                { 22, 6, 8 },
-                { 13, 3, 6 }
+        int[] schedules = { 700, 800, 1100 };
+        int[][] timelogs = {
+                { 710, 2359, 1050, 700, 650, 631, 659 },
+                { 800, 801, 805, 800, 759, 810, 809 },
+                { 1105, 1001, 1002, 600, 1059, 1001, 1100 }
         };
+        int startday = 5;
 
-        for (int[] testcase : testCase) {
-            int answer = rv.solution(testcase[0], testcase[1], testcase[2]);
-            System.out.printf("{ %d, %d, %d }: %d\n", testcase[0], testcase[1], testcase[2], answer);
-        }
+        System.out.printf("%d\n", rv.solution(schedules, timelogs, startday));
+    }
+    static int toMinutes(int time) {
+        return (time / 100) * 60 + time % 100;
     }
 
-    public int solution(int n, int w, int num) {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
         int answer = 0;
 
-        // 1. completeRow(꽉 찬 줄 개수) 구하기
-        int completeRow = n / w;
+        for (int i = 0; i < schedules.length; i++) {
+            int commuteTime = toMinutes(schedules[i] + 10);       // change to minutes to easier compare
+            int safeCount = 0;
+            for (int j = 0; j < timelogs[i].length; j++) {
+                if ((startday + j)%7 == 6 ||( startday + j)%7 == 0) continue;
+                if (commuteTime >= toMinutes(timelogs[i][j])) safeCount++;
+            }
+            if (safeCount >= 5) answer++;
+        }
 
-        // 2. numRow(num이 속한 줄) 구하기
-        int numRow = (num - 1) / w + 1;
-        int lastRow = (n - 1) / w + 1;
-
-        // 3. 최소 answer = completeRow - numRow + 1
-        answer = completeRow - numRow + 1;
-
-        // 4. 맨 윗 줄이 꽉찬 줄일 경우 (n % w == 0) => answer 반환
-        if (n % w == 0) return answer;
-
-        // 5. numDistance, lastDistance 정의 (가장 왼쪽 세로 줄로부터의 거리)
-        boolean isNumDirectionLeft = (numRow % 2 == 0);
-        boolean isLastDirectionLeft = (lastRow % 2 == 0);
-
-        int numDistance = (num - 1) % w;
-        if (isNumDirectionLeft) numDistance = (w - 1) - numDistance;
-
-        int lastDistance = (n - 1) % w;
-        if (isLastDirectionLeft) lastDistance = (w - 1) - lastDistance;
-
-        // 6. answer의 1 증가 조건:
-        // ((last줄 좌향) and (numDistance > lastDistance))
-        // or ((last줄 우향) and (numDistance < lastDistance))
-        // or (numDistance == lastDistance)
-
-        if (((isLastDirectionLeft) && (numDistance > lastDistance)) ||
-                ((!isLastDirectionLeft) && (numDistance < lastDistance)) ||
-                (numDistance == lastDistance))
-            answer += 1;
 
         return answer;
     }
